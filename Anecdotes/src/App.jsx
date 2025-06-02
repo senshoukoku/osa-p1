18324beb349a7d5d0d0285fb590e5a65aac144a4
senshoukoku <kurtset.22@gmail.com>
2025-06-02 11:49:53 +0800

    Anecdotes

---
+++ Anecdotes/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const handleRandClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+
+  const handleVoteClick = () => {
+    const votesCopy = [...votes]
+    votesCopy[selected] += 1
+    setVotes(votesCopy)
+  }
+
+  const maxVotes = Math.max(...votes)
+  const topAnecdoteIndex = votes.indexOf(maxVotes)
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <p><strong>Votes:</strong> {votes[selected]}</p>
+      <br/><button onClick={handleVoteClick}>Vote</button>
+      <button onClick={handleRandClick}>Show Random Anecdote</button>
+
+      <h2>Anecdote with Most Votes</h2><br />
+      {maxVotes === 0 ? (
+        <p>No Votes Yet</p>
+      ): (<div>
+        <p>{anecdotes[topAnecdoteIndex]}</p>
+        <p><strong>Votes: </strong>{maxVotes}</p>
+      </div>)}
+    </div>
+  )
+}
+
+export default App
